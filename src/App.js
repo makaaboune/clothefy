3bb23465b1b9b1cbd095edf3c2895531e5275bcd
AKAABOUNE Mohamed Amine <akaaboune.mohameamine@gmail.com>
2020-04-25 19:59:00 +0000

    feat(authentication): redirect to home page if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -47,7 +47,7 @@ class App extends React.Component {
         <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={AuthPage}/>
+        <Route exact path='/signin' render={()=>this.props.currentUser ? (<Redirect to="/"/>) : (<AuthPage/>)}/>
         </Switch>
       </div>
     );
@@ -55,8 +55,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps  = ({user}) =>({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
