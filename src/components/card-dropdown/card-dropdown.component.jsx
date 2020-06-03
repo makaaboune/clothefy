8f5205e5a9395ce1b4aeba1ff755de8ce6418a12
AKAABOUNE Mohamed Amine <akaaboune.mohameamine@gmail.com>
2020-06-03 23:01:09 +0100

    feat(cartItem): update cart item

--- src/components/card-dropdown/card-dropdown.component.jsx
+++ src/components/card-dropdown/card-dropdown.component.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import CustomButton from '../custom-button/CustomButton.component';
+import CartItem from '../cart-item/cart-item.component';
+import { connect } from 'react-redux';
 import './card-dropdown.style.scss';
 
-const CardDropdown = () => (
+const CardDropdown = ({cartItems}) => (
     <div className="card-dropdown">
         <div className="card-items">
-
+            {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
         </div>
         <CustomButton>checkout</CustomButton>
     </div>
 )
 
-export default CardDropdown;
+const mapStateToProps = ({card : {cartItems}}) => (
+    {cartItems}
+)
+
+export default connect(mapStateToProps)(CardDropdown);
